14e51645bfb6aca1c1de1079446666ed918ef770
Celso351 <98128754+Celso351@users.noreply.github.com>
2022-01-21 11:33:36 -0300

    Add files via upload

---
+++ Sorteiomania01.py
@@ -0,0 +1,20 @@
+print ('SORTEIOMANIA')
+print ('Números sorteados: ')
+import random
+random.seed( )
+l1 = print(random.sample(range(99), k=10))
+print ('Números escolhidos pelo jogador: ')
+
+random.seed( )
+l2 = print (random.sample(range(99), k=15))
+
+print ('Retorno: ')
+
+
+
+
+
+
+
+
+
